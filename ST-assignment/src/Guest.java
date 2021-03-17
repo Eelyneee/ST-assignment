845d8ecb343834a23fd0e7759f6561b8dc14e821
kai2128 <78791519+kai2128@users.noreply.github.com>
2021-03-17 10:08:28 +0800

    test

--- ST-assignment/src/Guest.java
+++ ST-assignment/src/Guest.java
@@ -3,6 +3,6 @@ package st.assignment;
 public class Guest extends User{
 
     public Guest(String name, Address address) {
-        super(name, address);
+        super(name, address);sdfsdf
     }
 }
